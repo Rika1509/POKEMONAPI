e63062d3cb3e48812819b273f38f44df2707585e
Rika1509 <bulma92@yahoo.it>
2024-08-04 17:33:16 +0200

    feat: added footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ interface Pokemon {
     type: {
       name: string;
     };
-  }>
+  }>;
   weight: number;
   height: number;
   name: string;
@@ -35,10 +35,10 @@ function App() {
   const [pokemonType, setPokemonType] = useState<string>("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPokemon(event.target.value.toLowerCase())
-  }
+    setPokemon(event.target.value.toLowerCase());
+  };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) =>   {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     getPokemon();
   };
@@ -47,7 +47,7 @@ function App() {
     try {
       const result = await axios.get<Pokemon>(`${apiKey}pokemon/${pokemon}`);
       console.log("🚀 ~ getPokemon ~ result:", result);
-      const data  = result.data;
+      const data = result.data;
       setPokemonType(data.types[0].type.name);
       setPokemonData([data]);
     } catch (error) {
@@ -61,66 +61,88 @@ function App() {
 
   return (
     <>
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <label>
-          <input
-            type="text"
-            onChange={handleChange}
-            placeholder="enter pokémon name"
-            value={pokemon}
-          />
-        </label>
-      </form>
-      {pokemonData.length > 0 && pokemonData.map((data) => (
-      <div className="pokemon container" key={data.id}>
-        <img src={data.sprites.front_default} alt={`${data.name} sprite`} />
-        <div className="divTable">
-          <div className="divTableBody">
-            <div className="divTableRow">
-              <div className="divTableCell">Type</div>
-              <div className="divTableCell">{pokemonType}</div>
-            </div>
-            <div className="divTableRow">
-              <div className="divTableCell">Height</div>
-              <div className="divTableCell">{data.height}"</div>
-            </div>
-            <div className="divTableRow">
-              <div className="divTableCell">Weight</div>
-              <div className="divTableCell">{data.weight} lbs</div>
-            </div>
-            <div className="divTableRow">
-              <div className="divTableCell">Abilities</div>
-              <div className="divTableCell">
-                <ul>
-                  {Children.toArray(
-                    data.abilities.map((ability) => (
-                      <li key={ability.ability.name}>
-                        {ability.is_hidden ? "hidden" : "normal"} : {ability?.ability.name}</li>
-                    ))
-                  )}
-                </ul>
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input
+              type="text"
+              onChange={handleChange}
+              placeholder="enter pokémon name"
+              value={pokemon}
+            />
+          </label>
+        </form>
+        {pokemonData.length > 0 &&
+          pokemonData.map((data) => (
+            <div className="pokemon container" key={data.id}>
+              <img
+                src={data.sprites.front_default}
+                alt={`${data.name} sprite`}
+              />
+              <div className="divTable">
+                <div className="divTableBody">
+                  <div className="divTableRow">
+                    <div className="divTableCell">Type</div>
+                    <div className="divTableCell">{pokemonType}</div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Height</div>
+                    <div className="divTableCell">{data.height}"</div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Weight</div>
+                    <div className="divTableCell">{data.weight} lbs</div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Abilities</div>
+                    <div className="divTableCell">
+                      <ul>
+                        {Children.toArray(
+                          data.abilities.map((ability) => (
+                            <li key={ability.ability.name}>
+                              {ability.is_hidden ? "hidden" : "normal"} :{" "}
+                              {ability?.ability.name}
+                            </li>
+                          ))
+                        )}
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Stats</div>
+                    <div className="divTableCell">
+                      <ul>
+                        {Children.toArray(
+                          data.stats.map((stat) => (
+                            <li key={stat.stat.name}>
+                              {stat?.stat.name} - base stat value:{" "}
+                              {stat?.base_stat}
+                            </li>
+                          ))
+                        )}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="divTableRow">
-              <div className="divTableCell">Stats</div>
-              <div className="divTableCell">
-                <ul>
-                  {Children.toArray(
-                    data.stats.map((stat) => (
-                      <li key={stat.stat.name}>
-                        {stat?.stat.name} - base stat value: {stat?.base_stat}
-                      </li>
-                    ))
-                  )}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+          ))}
+        <footer>
+          This project was coded by Federica Ferrante and is
+          <a href="https://github.com/Rika1509/POKEMONAPI" target="_blank">
+            {" "}
+            open-sourced on GitHub{" "}
+          </a>
+          and{" "}
+          <a
+            href="https://pokemon-react-pokedex-app.netlify.app/"
+            target="blank"
+          >
+            {" "}
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
-      ))}
-    </div>
     </>
   );
 }
